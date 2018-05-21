76d658f3d85a247b756c6efd5aa7b7865256ce20
jbimagine <joebates@jbimagine.com>
2018-05-20 23:14:59 -0700

    refactor flipsCard

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
-let cardsArray0, cardsArray1, fa;
+let cardsArray0, cardsArray1, fa, card, cardDeck;
+
+card = document.getElementsByClassName('card');
+deck = document.querySelector('.deck');
+cardDeck = document.getElementById('card-deck');
 
 fa = 'fa ';
 
@@ -57,42 +61,52 @@ cardsArray1 = shuffle(cardsArray1);
  */
 
 //Function that controls the flipping of the cards.  Else statement is there only for testing purposes
-function flipCard(card, theClasses) {
-	card = document.getElementsByClassName('card');
-	theClasses = document.querySelectorAll('.deck .card');
 
-	for (let i = 0; i < card.length; i++) {
-		card[i].addEventListener('click', () => {
-			if (!theClasses[i].classList.contains('show')) {
-				card[i].style.transform = 'rotateY(180deg)';
-				card[i].classList.add('show', 'open');
-				console.log(cardsArray1[i]);
-			} //else {
-			//card[i].style.transform = 'rotateY(0deg)';
-			//card[i].classList.remove('show', 'open');
-			//}
-		});
-	}
+function displayCard() {
+	this.classList.toggle('open');
+	this.classList.toggle('show');
 }
 
+function flipsCard() {
+	let selectedCard = '';
+	let matchedCards = [];
+	let flippedCards = [];
+
+	cardDeck.addEventListener('click', function(evt) {
+		cardTarget = evt.target;
+		if (cardTarget && cardTarget.nodeName === 'LI' && flippedCards.length < 2) {
+			cardTarget.classList.add('open', 'show');
+			cardTarget.style.transform = 'rotateY(180deg)';
+			selectedCard = cardTarget.children[0].classList[1];
+			flippedCards.push(selectedCard);
+			console.log(flippedCards);
+		}
+		if (flippedCards[0] === flippedCards[1]) {
+			matchedCards.push(flippedCards[0], flippedCards[1]);
+			macthed();
+		} else {
+			unmatched();
+		}
+	});
+}
+
+function matched() {}
+
+function unmatched() {}
+
 //Dynamically creates the html cards
-function deckOfCards(card, cardDeck) {
+function deckOfCards(card) {
 	let cardStr;
 	let iconPic;
-	cardDeck = document.querySelector('.deck');
 	let cards = '';
-
 	for (let card of cardsArray1) {
 		cards += `<li class='card'><i class ='${fa + card}'></i></li>`;
 	}
-	console.log(cards);
-	cardDeck.innerHTML = cards;
+	deck.innerHTML = cards;
 	//add in the flipcard function
-	flipCard();
+	flipsCard();
 }
 deckOfCards();
-
-function openedCards(cards) {}
 
 //restart the game
 function restartGame(restart) {
